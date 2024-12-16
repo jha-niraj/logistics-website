7fcdcd5557a68921802080d74f4d7237f8a975fa
Niraj Jha <109269916+jha-niraj@users.noreply.github.com>
2024-12-16 17:28:42 +0530

    Update the config.ts to redirect to the production server

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://logistics-center.onrender.com/api/v1";
-export const BASE_URL = "http://localhost:3002/api/v1";
+export const BASE_URL = "https://logistics-center.onrender.com/api/v1";
+// export const BASE_URL = "http://localhost:3002/api/v1";
